0e175c02f8871cd40b22a03923434172762ea7f0
timmson <timmson666@mail.ru>
2020-04-11 02:25:40 +0300

    Step 1

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # bdd-and-tdd
 ATDD and TDD example
+
+
+![A-TDD](https://dzone.com/storage/temp/7012527-atdd-workflow.jpg)
+
+[Read More](https://dzone.com/articles/acceptance-test-driven-development-closing-the-com)
---
+++ src/test/java/ru/timmson/workshop/bddandtdd/CucumberRunner.java
@@ -0,0 +1,10 @@
+package ru.timmson.workshop.bddandtdd;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources", plugin = {"pretty", "html:target/cucumber"}, strict = true)
+public class CucumberRunner {
+}
