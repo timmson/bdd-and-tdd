7745079152aa2734b0c0e22644c85210468c8bae
timmson <timmson666@mail.ru>
2020-04-11 03:14:54 +0300

    Step 1

--- src/test/java/ru/timmson/workshop/bddandtdd/Service.java
+++
@@ -1,7 +0,0 @@
-package ru.timmson.workshop.bddandtdd;
-
-class Service {
-    public static int calculate(int orderAmount, int deliveryAmount) {
-        return orderAmount + deliveryAmount;
-    }
-}
