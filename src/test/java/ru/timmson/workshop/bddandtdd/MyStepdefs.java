2369aeed5af7c1b2e625c7bb5b831012ca262707
timmson <timmson666@mail.ru>
2020-04-11 03:02:39 +0300

    Step 1

--- src/test/java/ru/timmson/workshop/bddandtdd/MyStepdefs.java
+++ src/test/java/ru/timmson/workshop/bddandtdd/MyStepdefs.java
@@ -2,17 +2,27 @@ package ru.timmson.workshop.bddandtdd;
 
 import io.cucumber.java8.En;
 
+import static org.junit.Assert.assertEquals;
+
 public class MyStepdefs implements En {
 
+    String clientType;
+    Integer cartSum;
+    Integer orderSum = 0;
+
     public MyStepdefs() {
+
         Given("{string} и {int}", (String clientType, Integer cartSum) -> {
+            this.clientType = clientType;
+            this.cartSum = cartSum;
         });
 
         When("Проводим расчет", () -> {
+            //
         });
 
         Then("Получаем {int}", (Integer orderSum) -> {
-
+            assertEquals(orderSum, this.orderSum);
         });
 
     }
