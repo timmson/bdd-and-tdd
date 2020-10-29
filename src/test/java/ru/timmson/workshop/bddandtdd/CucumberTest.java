0c3514e14d924069a9698ef15e12e737b8e01031
timmson <timmson666@mail.ru>
2020-10-29 15:14:07 +0300

    Some refactoring

--- src/test/java/ru/timmson/workshop/bddandtdd/CucumberTest.java
+++ src/test/java/ru/timmson/workshop/bddandtdd/CucumberTest.java
@@ -1,7 +1,6 @@
 package ru.timmson.workshop.bddandtdd;
 
 import io.cucumber.junit.platform.engine.Cucumber;
-import org.springframework.boot.test.context.SpringBootTest;
 
 @Cucumber
 public class CucumberTest {
